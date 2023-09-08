366b74f93477c61871cb1b6875ab1bc5ce102a73
Aryan Sahu <aryansahu.vibranium@gmail.com>
2023-09-08 19:34:21 +0530

    Sets in python

---
+++ sets.py
@@ -0,0 +1,23 @@
+cities = {"Tokyo","Delhi", "Singapore","Bejeing"}
+cities2 = {"Tokyo"}
+
+# cities3 = cities.intersection(cities2)
+# print(cities3)
+
+# cities3 = cities.union(cities2)
+# print(cities3)
+
+# cities3 = cities.symmetric_difference(cities2)
+# cities4  = cities2.symmetric_difference(cities)
+# print(cities3)
+# print(cities4)
+
+# print(cities.isdisjoint(cities2)) #It is the function which checks whether all the elements are different or not
+
+# print(cities.issuperset(cities2)) All the elements of cities2 is in cities
+
+item = cities.pop()
+print(item)
+
+
+
