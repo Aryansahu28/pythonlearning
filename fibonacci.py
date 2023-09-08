21ad377dc6bbefcf33db868d37c1b493816bec6c
Aryan Sahu <aryansahu.vibranium@gmail.com>
2023-09-08 19:19:42 +0530

    f-string is here

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# def fibonacci(n):
+#     if(n==0):
+#         print(0)
+#     elif(n==1):
+#         print("0 1")
+#     elif(n>1):
+#         fibonacci(n-1)+fibonacci(n-2)
+#         print(n)
+
+
+# # fibonacci(5)
+harry = {1}
+print(type(harry))
