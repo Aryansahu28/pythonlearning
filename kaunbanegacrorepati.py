9e0e396c926cf6ce79f87df97d70bbc87e90407b
Aryan Sahu <aryansahu.vibranium@gmail.com>
2023-09-11 16:32:24 +0530

    kaun banega crorepati

--- kaunbanegacrorepati.py
+++ kaunbanegacrorepati.py
@@ -1,20 +1,41 @@
-Questions = ["Who is the Prime Minister of India","What is the National Animal of India"]
-Answer = ["Narendra Modi","Peacock"]
-points = 0
-print(Questions[0])
-for i in range(0,2):
-    print(Questions[i],"\n")
-    Your_Answer = input("Your answer is ")
-    if(Your_Answer == Answer[i] ):
-        print("Your answer is correct\n")
-        points = points + 1
+question = [
+    [
+        "Who is the Prime Minister of Bharat","Narendra Modi","Jawaharlal Nehru","Manmohan Singh","Pratibha Patil"
+        ],
+    [
+        "Who is the President of Bharat","Draupati Murmu","Pratibha Patel","nehru","Ravi"
+        ],
+    [
+        "Who is the Prime Minister of Bharat","Narendra Modi","Jawaharlal Nehru","Manmohan Singh","Pratibha Patil"
+        ],
+    [
+        "Who is the President of Bharat","Draupati Murmu","Pratibha Patel","nehru","Ravi"
+        ],
+    [
+        "Who is the Prime Minister of Bharat","Narendra Modi","Jawaharlal Nehru","Manmohan Singh","Pratibha Patil"
+        ],
+    [
+        "Who is the President of Bharat","Draupati Murmu","Pratibha Patel","nehru","Ravi"
+        ],
+    [
+        "Who is the Prime Minister of Bharat","Narendra Modi","Jawaharlal Nehru","Manmohan Singh","Pratibha Patil"
+        ],
+    [
+        "Who is the President of Bharat","Draupati Murmu","Pratibha Patel","nehru","Ravi"
+        ]
+    ]
+money = 0
+price = [1000,2000,3000,5000,10000,20000,35000,50000,120000,320000]
+for i in range(0,len(question)):
+    questions = question[i]
+    print(f"\nPrize money for question {i+1} is {price[i]}\n{question[i][0]}")
+    print(f'''A.{questions[1]}         B.{questions[2]}
+              C.{questions[3]}       D.{questions[4]}''')
+    reply = int(input("Enter your option as (1-4): "))
+    if(reply == 1):
+        print("Your answer is correct")
 
-    
+    else:
+        print("Wrong answer")
+        break
 
-print("Your point is ",points)
-
-# print("Write any one of the option")
-# print(Questions[0])
-# print("[A] Narendra Modi                          [B] Manmohan Singh")
-# print("[C] Indira Gandhi                          [D] Jawaharlal Nehru")
-# if()
